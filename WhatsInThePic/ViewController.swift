c3640f248c80344c45af75221b0bdb2ab07274d1
gwojcik <zgagato@mac.com>
2019-07-02 14:33:29 +0200

    Initial Commit

---
+++ WhatsInThePic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsInThePic
+//
+//  Created by zgaga on 2-07-2019.
+//  Copyright © 2019 Hime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
